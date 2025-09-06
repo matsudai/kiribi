e5f3d22b17f595cae113a09004b001a2b267ecf4
matsudai <matsudai.social.1131@gmail.com>
2025-09-06 23:55:48 +0900

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.0.1] - 2025-09-06
+
+- Initial release
---
+++ lib/kiribi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "kiribi/version"
+
+module Kiribi
+  class Error < StandardError; end
+  # Your code goes here...
+end
